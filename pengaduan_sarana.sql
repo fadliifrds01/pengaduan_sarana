8e4c7a928d354180d85ba5634e207bf764a539ee
FadliFarodis <fadlifrdss01@gmail.com>
2026-02-03 11:05:05 +0700

    Update database

--- pengaduan_sarana.sql
+++ pengaduan_sarana.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 6.0.0-dev+20250908.be01432c56
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 14 Jan 2026 pada 19.58
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: Feb 03, 2026 at 04:03 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,16 +24,16 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `admin`
+-- Table structure for table `admin`
 --
 
 CREATE TABLE `admin` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `admin`
+-- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`username`, `password`) VALUES
@@ -42,44 +42,45 @@ INSERT INTO `admin` (`username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `aspirasi`
+-- Table structure for table `aspirasi`
 --
 
 CREATE TABLE `aspirasi` (
-  `id_aspirasi` int(11) NOT NULL,
-  `nis` char(10) DEFAULT NULL,
-  `id_kategori` int(11) DEFAULT NULL,
-  `lokasi` varchar(100) DEFAULT NULL,
-  `keterangan` text DEFAULT NULL,
-  `foto` varchar(255) DEFAULT NULL,
-  `status` enum('menunggu','proses','selesai') DEFAULT 'menunggu',
-  `feedback` text DEFAULT NULL,
-  `tanggal_pelaporan` timestamp NOT NULL DEFAULT current_timestamp()
+  `id_aspirasi` int NOT NULL,
+  `nis` char(10) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `id_kategori` int DEFAULT NULL,
+  `lokasi` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `keterangan` text COLLATE utf8mb4_general_ci,
+  `foto` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `status` enum('menunggu','proses','selesai') COLLATE utf8mb4_general_ci DEFAULT 'menunggu',
+  `feedback` text COLLATE utf8mb4_general_ci,
+  `tanggal_pelaporan` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `aspirasi`
+-- Dumping data for table `aspirasi`
 --
 
 INSERT INTO `aspirasi` (`id_aspirasi`, `nis`, `id_kategori`, `lokasi`, `keterangan`, `foto`, `status`, `feedback`, `tanggal_pelaporan`) VALUES
 (1, '12345', 1, 'Lab komputer 1', 'komputer meledak', NULL, 'selesai', 'siap', '2026-01-14 03:57:50'),
-(2, '12345', 3, 'Lab RPL', 'Komputer meledak dan megeluarkan percikan api', NULL, 'proses', 'terimakasih, komputer sedang dalam perbaikan', '2026-01-14 04:08:55'),
+(2, '12345', 3, 'Lab RPL', 'Komputer meledak dan megeluarkan percikan api', NULL, 'selesai', 'Sudah selesai ya', '2026-01-14 04:08:55'),
 (3, '12345', 3, 'lab tkj', 'wifi mati', NULL, 'selesai', 'sudah beres', '2026-01-14 05:50:00'),
-(4, '12345', 3, 'Lab RPL Timur', 'PC meledak dan mengeluarkan percikan api sehingga PC mengalami kerusakan dan tidak menyala', NULL, 'selesai', 'baik sedang dalam penyelidikan', '2026-01-14 06:51:09');
+(4, '12345', 3, 'Lab RPL Timur', 'PC meledak dan mengeluarkan percikan api sehingga PC mengalami kerusakan dan tidak menyala', NULL, 'selesai', 'Done ya bang', '2026-01-14 06:51:09'),
+(5, '12345', 2, 'LAB RPL 1', 'Ada makanan tumpah', NULL, 'selesai', 'Done Ya', '2026-02-03 03:06:13');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kategori`
+-- Table structure for table `kategori`
 --
 
 CREATE TABLE `kategori` (
-  `id_kategori` int(11) NOT NULL,
-  `keterangan_kategori` varchar(100) NOT NULL
+  `id_kategori` int NOT NULL,
+  `keterangan_kategori` varchar(100) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `kategori`
+-- Dumping data for table `kategori`
 --
 
 INSERT INTO `kategori` (`id_kategori`, `keterangan_kategori`) VALUES
@@ -91,21 +92,22 @@ INSERT INTO `kategori` (`id_kategori`, `keterangan_kategori`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `siswa`
+-- Table structure for table `siswa`
 --
 
 CREATE TABLE `siswa` (
-  `nis` char(10) NOT NULL,
-  `kelas` varchar(20) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `nis` char(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `kelas` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `siswa`
+-- Dumping data for table `siswa`
 --
 
 INSERT INTO `siswa` (`nis`, `kelas`, `password`) VALUES
 ('12345', 'XII RPL 1', 'siswa123'),
+('1244', 'XII RPL 5', '00000'),
 ('555', 'XII RPL', '555');
 
 --
@@ -113,13 +115,13 @@ INSERT INTO `siswa` (`nis`, `kelas`, `password`) VALUES
 --
 
 --
--- Indeks untuk tabel `admin`
+-- Indexes for table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`username`);
 
 --
--- Indeks untuk tabel `aspirasi`
+-- Indexes for table `aspirasi`
 --
 ALTER TABLE `aspirasi`
   ADD PRIMARY KEY (`id_aspirasi`),
@@ -127,39 +129,39 @@ ALTER TABLE `aspirasi`
   ADD KEY `id_kategori` (`id_kategori`);
 
 --
--- Indeks untuk tabel `kategori`
+-- Indexes for table `kategori`
 --
 ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indeks untuk tabel `siswa`
+-- Indexes for table `siswa`
 --
 ALTER TABLE `siswa`
   ADD PRIMARY KEY (`nis`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `aspirasi`
+-- AUTO_INCREMENT for table `aspirasi`
 --
 ALTER TABLE `aspirasi`
-  MODIFY `id_aspirasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_aspirasi` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `kategori`
+-- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_kategori` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `aspirasi`
+-- Constraints for table `aspirasi`
 --
 ALTER TABLE `aspirasi`
   ADD CONSTRAINT `aspirasi_ibfk_1` FOREIGN KEY (`nis`) REFERENCES `siswa` (`nis`),
